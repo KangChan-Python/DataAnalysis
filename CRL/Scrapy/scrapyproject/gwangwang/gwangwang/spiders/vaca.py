76ff15b69ff0a94b7f55472fc6a7319babbf0f5d
KangChan-Python <kch961203@gmail.com>
2020-07-26 01:05:41 +0900

    1

---
+++ CRL/Scrapy/scrapyproject/gwangwang/gwangwang/spiders/vaca.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class VacaSpider(scrapy.Spider):
+    name = 'vaca'
+    # start_urls = ['https:/korean.visitkorea.or.kr/main/cs_main.do/']
+
+    def start_request
